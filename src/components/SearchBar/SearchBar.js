26f369f868890060f1bda5e5cbb4abc2838dc289
LesiaBelinska <lm.kudak@gmail.com>
2021-11-04 18:15:10 +0200

    add SearchBar styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,45 +1,37 @@
 import { ImSearch } from 'react-icons/im';
 import { useState } from 'react';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 import PropTypes from 'prop-types';
-//import s from './Searchbar.module.css';
+import s from './SearchBar.module.css';
 
 export default function Searchbar({ onSubmit }) {
   const [filmName, setFilmName] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
-
-    if (filmName.trim() === '') {
-      toast('Please enter search query');
-      return;
-    }
-
     onSubmit(filmName);
-
     setFilmName('');
   };
 
   return (
-    <header className='{s.searchbar}'>
-      <form className='{s.searchForm}' onSubmit={handleSubmit}>
+    <div className={s.searchbar}>
+      <form className={s.searchForm} onSubmit={handleSubmit}>
         <input
           value={filmName}
           onChange={event =>
             setFilmName(event.currentTarget.value.toLowerCase())
           }
-          className='{s.searchFormInput}'
+          className={s.searchFormInput}
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search films"
         />
-        <button type="submit" className='{s.searchFormButton}'>
+        <button type="submit" className={s.searchFormButton}>
           <ImSearch />
         </button>
       </form>
-    </header>
+    </div>
   );
 }
 
