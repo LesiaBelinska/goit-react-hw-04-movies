47e275685881e5ede7fb4d219f01c034fad161bc
LesiaBelinska <lm.kudak@gmail.com>
2021-11-05 16:21:58 +0200

    add styles

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -11,20 +11,20 @@ export default function Reviews({ movieId }) {
   }, [movieId]);
 
   return (
-    <div className='{s.wrapper}'>
+    <div className={s.wrapper}>
       {reviews.length > 0 ? (
         <>
-          <ul className='{s.list}'>
+          <ul className={s.list}>
             {reviews.map((item, index) => (
-              <li key={index} className='{s.item}'>
-                <p className='{s.title}'> {item.author}</p>
+              <li key={index} className={s.item}>
+                <p className={s.title}> {item.author}</p>
                 <p> {item.content}</p>
               </li>
             ))}
           </ul>
         </>
       ) : (
-        <p className='{s.text}'>No reviews to show</p>
+        <p className={s.text}>No reviews to show</p>
       )}
     </div>
   );
